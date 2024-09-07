17eec385cdffbad65f5de196471b327ba01f2305
Lucas Roe <lroe2930@gmail.com>
2024-09-07 02:54:24 -0400

    Swapping to Manual LSP
    Needs cleanup but is functional

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,11 +11,31 @@ vim.keymap.set('n', '<C-d>', ':d_<enter>', {desc = 'Delete Line'})
 vim.keymap.set('i', '<C-s>', '<C-o>:w<enter>', {desc = 'Save'})
 vim.keymap.set('i', '<C-r>', function() vim.lsp.buf.rename() end, {desc = 'Rename'})
 vim.keymap.set('n', '<C-r>', function() vim.lsp.buf.rename() end, {desc = 'Rename'})
-vim.keymap.set('n', '<C-`>', ':belowright split | resize 10 | term <enter><Home>i', {desc = 'Open Terminal'})
-vim.keymap.set('i', '<C-`>', '<C-o>:belowright split | resize 10 | term <enter>', {desc = 'Open Terminal'})
 vim.keymap.set('i', '<F2>', '<C-o>:NvimTreeToggle <enter>', {desc = 'Toggle Tree'})
 vim.keymap.set('n', '<F2>', ':NvimTreeToggle <enter>', {desc = 'Toggle Tree'})
 
+-- LSP Keybinds
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'LSP Actions',
+	callback = function(event)
+		local bufmap = function(mode, lhs, rhs)
+			local opts = {buffer = event.buf}
+			vim.keymap.set(mode, lhs, rhs, opts)
+		end
 
+	-- Code Completion
+	bufmap('i', '<C-Space>', '<C-x><C-o>')
 
+	-- Show Hover Docs
+	bufmap('n', '<F1>', '<cmd>lua vim.lsp.buf.hover()<cr>')
+	
+	-- show refs
+	bufmap('n', '<M-]>', '<cmd>lua vim.lsp.buf.references()<cr>')
+	
+	-- Renames all references to the symbol under the cursor
+  bufmap('n', '<C-r>r', '<cmd>lua vim.lsp.buf.rename()<cr>')
 
+	-- Show Quick Actions
+	bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+	end
+})
