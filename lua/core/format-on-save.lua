0c299c000c10b6dccbb74353313f921a0e14dc0f
lroe <lroe2930@gmail.com>
2024-09-07 18:43:50 -0400

    added format imports on save

--- lua/core/format-on-save.lua
+++ lua/core/format-on-save.lua
@@ -1,5 +1,19 @@
 local fmt_group = vim.api.nvim_create_augroup('autoformat_cmds', {clear = true})
 
+local function organize_imports(client, bufnr)
+  local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+  params.context = { only = { "source.organizeImports" } }
+
+  local resp = client.request_sync("textDocument/codeAction", params, 3000, bufnr)
+  for _, r in pairs(resp and resp.result or {}) do
+    if r.edit then
+      vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+    else
+      vim.lsp.buf.execute_command(r.command)
+    end
+  end
+end
+
 local function setup_autoformat(event)
   local id = vim.tbl_get(event, 'data', 'client_id')
   local client = id and vim.lsp.get_client_by_id(id)
@@ -10,7 +24,8 @@ local function setup_autoformat(event)
   vim.api.nvim_clear_autocmds({group = fmt_group, buffer = event.buf})
 
   local buf_format = function(e)
-    vim.lsp.buf.format({
+  	organize_imports(client, e.buf)
+		vim.lsp.buf.format({
       bufnr = e.buf,
       async = false,
       timeout_ms = 10000,
